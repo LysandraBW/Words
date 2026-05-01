6a97c41c45adacd3e3fd8e3f3b96ff8e8d4570b5
Lysandra <81883243+LysandraBW@users.noreply.github.com>
2026-05-01 01:28:40 -0400

    Tried to use WordNet, kind of worked? I don't know... and that's okay

--- server/src/utilities/words.ts
+++ server/src/utilities/words.ts
@@ -2,6 +2,46 @@ import 'dotenv/config';
 import retry from 'async-retry';
 import * as lancedb from "@lancedb/lancedb";
 import words from 'an-array-of-english-words' with { type: 'json' };
+import natural from 'natural';
+
+
+const wordnet = new natural.WordNet();
+
+
+function lookup(word: string): Promise<natural.DataRecord[]> {
+    return new Promise((res, rej) => {
+        wordnet.lookup(word, (results) => res(results));
+    });
+}
+
+function get(offset: number, pos: string): Promise<natural.DataRecord> {
+    return new Promise((res, rej) => {
+        wordnet.get(offset, pos, (results) => res(results));
+    });
+}
+
+export async function getSimilarWordsWordNet(word: string): Promise<string[]> {
+    const entries = await lookup(word);
+    const similars: string[] = []
+
+    for (const entry of entries) {
+        for (const ptr of entry.ptrs) {
+            if (ptr.pointerSymbol !== '@')
+                continue;
+
+            const parentEntry = await get(ptr.synsetOffset, ptr.pos);
+            for (const parentPtr of parentEntry.ptrs) {
+                if (parentPtr.pointerSymbol !== '~')
+                    continue;
+
+                const childEntry = await get(parentPtr.synsetOffset, parentPtr.pos);
+                similars.push(childEntry.lemma.replaceAll('_', ' '));
+            }
+        }
+    }
+
+    return similars;
+}
 
 
 export async function getSimilarWords(word: string, n: number = 20) {
@@ -20,7 +60,7 @@ export async function getSimilarWords(word: string, n: number = 20) {
 
 
 export async function getRandomWords(word: string = "", wordDefinition: string = "", n: number = 3): Promise<[string, string][]> {
-    const pool = await getSimilarWords(word.toLowerCase(), Math.max(50, n)) || words;
+    const pool = await getSimilarWordsWordNet(word) || await getSimilarWords(word.toLowerCase(), Math.max(50, n)) || words;
     
     // Shuffle Indices
     const poolIndices = [...Array(pool.length)].map((e, i) => i);
@@ -41,15 +81,14 @@ export async function getRandomWords(word: string = "", wordDefinition: string =
 
                 const randIndex = poolIndices[i + offset] as number;
                 const randWord = pool[randIndex];
+                if (!randWord)
+                    throw new Error('Random Index Out of Bounds');
 
                 let url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
                 url += `${randWord}?key=${process.env.MERRIAM_WEBSTER_API_KEY_DICTIONARY}`;
 
                 const data = await (await fetch(url)).json();
                 const defs = data[0].shortdef.filter((definition: string) => definition !== wordDefinition);
-                // Using all the definitions allows for the weirder sort
-                // to be introduced.
-                // const defs = data.reduce((accumulator: any[], currentEntry: any) => [...accumulator, ...currentEntry.shortdef], []).filter((definition: string) => definition !== wordDefinition);
 
                 if (!defs || !defs.length) {
                     offset += 1;
